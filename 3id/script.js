4ee4253722ce8376a728f28ce86ed3836d4887a9
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-05-09 10:50:10 +0000

    Versione Base vista col Bernini

--- 3id/script.js
+++ 3id/script.js
@@ -6,4 +6,9 @@ function mostraContenutoCasellaA(){
     alert(typeof (n));
     alert(n+1);
 }
+function testCampo(stringa, pattern) {
 
+    return new RegExp(pattern).test(stringa);
+  
+}
+alert(testCampo("ciao@ciao.it", "^[a-z0-9]+@[a-z0-9]+.(it|com|biz)$"));
